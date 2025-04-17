005430bad72b02fa33401dd8d57b28390781b69a
imslv <ivan.maslov.media@gmail.com>
2025-04-17 22:28:51 +0200

    Initial Commit

---
+++ Planujemy/PlanujemyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlanujemyApp.swift
+//  Planujemy
+//
+//  Created by Ivan Maslov on 17/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct PlanujemyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
